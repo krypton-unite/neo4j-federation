f3bee3eeec27028425b14db6ea01836355783b17
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-26 11:03:21 +0000

    :construction: Development path.

--- scripts/install-neo4j.sh
+++ scripts/install-neo4j.sh
@@ -10,8 +10,9 @@ set -xe
 if [ ! -d "neo4j/data/databases/graph.db" ]; then
     mkdir -p neo4j
     wget dist.neo4j.org/neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz
-    tar -xzf neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz -C neo4j --strip-components 1
-    neo4j/bin/neo4j-admin set-initial-password letmein
+    umask 022
+    sudo tar -xzf neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz -C neo4j --strip-components 1
+    neo4j/bin/neo4j-admin set-initial-password $NEO4J_PASSWORD
     curl -L https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/$APOC_VERSION/apoc-$APOC_VERSION-all.jar > ./neo4j/plugins/apoc-$APOC_VERSION-all.jar
     wget https://datastores.s3.amazonaws.com/recommendations/v$DATASTORE_VERSION/recommendations.db.zip
     sudo apt-get install unzip
