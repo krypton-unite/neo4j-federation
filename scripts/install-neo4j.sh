141ec7df5d60fb21fd72422b30ea7012c1dc5f15
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-26 15:31:33 +0000

    :construction: Development path.

--- scripts/install-neo4j.sh
+++ scripts/install-neo4j.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+yes | sudo apt-get install fdupes unzip
+
+cache=.download_cache
+
+get_latest_download() {
+  cd $cache
+  latest_download="$(ls -r $1 | head -1)"
+  echo "THERE: $latest_download :THERE"
+  cd ..
+}
+
+download_and_get_latest() {
+  get_latest_download $2*
+
+  echo "HERE: $latest_download :HERE"
+  if [ ! "$latest_download" ]; then
+    wget $1/$2 -P $cache
+    get_latest_download $2*
+  fi
+}
+
+delete_cache_duplicates(){
+  fdupes -rdN $cache
+}
+
 if [ ! -f ../.env ]
 then
   export $(cat .env | xargs)
@@ -8,18 +33,28 @@ fi
 set -xe
 
 if [ ! -d "neo4j/data/databases/graph.db" ]; then
-    mkdir -p neo4j
-    wget dist.neo4j.org/neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz
-    tar -xzf neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz -C neo4j --strip-components 1
+    if [ ! -L neo4j ]; then
+      mkdir -p -- neo4j
+      mkdir -p -- $cache
+    fi
+    neo4j=neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz 
+    download_and_get_latest dist.neo4j.org $neo4j
+
+    sudo tar -xzf $cache/$latest_download -C neo4j --strip-components 1
+
+    neo4j/bin/neo4j-admin set-default-admin $NEO4J_USER
     neo4j/bin/neo4j-admin set-initial-password $NEO4J_PASSWORD
-    curl -L https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/$APOC_VERSION/apoc-$APOC_VERSION-all.jar > ./neo4j/plugins/apoc-$APOC_VERSION-all.jar
-    wget https://datastores.s3.amazonaws.com/recommendations/v$DATASTORE_VERSION/recommendations.db.zip
-    sudo apt-get install unzip
-    unzip recommendations.db.zip
+
+    apoc=apoc-$APOC_VERSION-all.jar
+    download_and_get_latest https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/$APOC_VERSION $apoc
+    cp $cache/$apoc ./neo4j/plugins/$apoc
+    
+    recommendations=recommendations.db.zip
+    download_and_get_latest https://datastores.s3.amazonaws.com/recommendations/v$DATASTORE_VERSION $recommendations
+
+    unzip -o $cache/$latest_download
+    delete_cache_duplicates
     mv recommendations.db neo4j/data/databases/graph.db
-    # clean up
-    rm neo4j-$NEO4J_DIST-$NEO4J_VERSION-unix.tar.gz*
-    rm recommendations.db.zip*
     rm __MACOSX* -r
 else
     echo "Database is already installed, skipping"
