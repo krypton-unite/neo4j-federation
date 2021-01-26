1a227c0539e6c7f37f7054158da01e95c218a991
Daniel Kaminski de Souza <daniel@kryptonunite.com>
2021-01-26 11:09:44 +0000

    :construction: Development path.

--- src/gateway.js
+++ src/gateway.js
@@ -14,10 +14,7 @@ dotenv.config();
 
 const driver = neo4j.driver(
   process.env.NEO4J_URI || 'bolt://localhost:7687',
-  neo4j.auth.basic(
-    process.env.NEO4J_USER || 'neo4j',
-    process.env.NEO4J_PASSWORD
-  )
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 );
 
 // Start Accounts
